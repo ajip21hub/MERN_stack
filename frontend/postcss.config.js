ce25e382416aa6b03bfe6c534dc64efc7961d424
ajipproject21 <ajipproject21@gmail.com>
2024-08-01 06:11:40 +0700

    fix: solve to ui error

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,12 +1,21 @@
 // postcss.config.js
 module.exports = {
-    plugins: {
-      'postcss-preset-env': {
-        stage: 1,
-        features: {
-          'nesting-rules': true
-        }
-      },
-      'postcss-calc': {}
-    }
+    plugins: [
+        // PostCSS Preset Env lets you convert modern CSS into something most browsers can understand.
+        require('postcss-preset-env')({
+          stage: 1, // Enable modern CSS features
+          features: {
+            'nesting-rules': true // Enable CSS nesting
+          }
+        }),
+        // PostCSS Calc allows you to reduce calc() references when possible
+        require('postcss-calc')({
+          warnWhenCannotResolve: true, // Warn if any calc() expressions cannot be resolved
+          preserve: false // Remove the original calc() expressions
+        }),
+        // CSSNano is used for minifying the CSS
+        require('cssnano')({
+          preset: 'default'
+        })
+      ]
   };
