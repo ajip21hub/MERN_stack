30be2bf6a823139db56413301062555837b99340
ajipproject21 <ajipproject21@gmail.com>
2024-08-01 15:03:59 +0700

    update port server

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const emailRoute = require('./routes/emailRoutes')
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // connectDB
 connectDB();
